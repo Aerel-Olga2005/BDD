ca2ce0d1786bcad9069b08ffcccd17e0ebde2586
Helga NN <o9351030@gmail.com>
2026-05-15 22:38:08 +0300

    Problem of extractBalance

--- src/test/java/ru/netology/bdd/page/DashboardPage.java
+++ src/test/java/ru/netology/bdd/page/DashboardPage.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
-    private final String balanceStart = "баланс:";
+    private final String balanceStart = ", баланс: ";
     private final String balanceFinish = " р. ";
     private final SelenideElement heading = $("[data-test-id=dashboard]");
     private final ElementsCollection cards = $$(".list__item div");
@@ -43,7 +43,7 @@ public class DashboardPage {
     private int extractBalance(String text) {
         var start = text.indexOf(balanceStart);
         var finish = text.indexOf(balanceFinish);
-        var value = text.substring(start  + balanceStart.length(), finish);
+        var value = text.substring(start + balanceStart.length(), finish);
         return Integer.parseInt(value);
     }
 
